602f3497ecea8f4f01896024ee8f9a0f5820c580
ivanlandry <58294595+ivanlandry@users.noreply.github.com>
2024-04-03 16:53:59 -0400

    integeration de template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,32 @@
 <!DOCTYPE html>
-<html lang="fr-CA">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('titre')</title>
-</head>
-
-<body>
-<!-- Mettre la NavBar et toutes les entêtes du site ici -->
-
-
-@yield('contenu')
-
-<!-- Mettre le footer -->
-</body>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Modern Business - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+    </head>
+    <body class="d-flex flex-column h-100">
+        <main class="flex-shrink-0">
+            <!-- Navigation-->
+            @include("partials.navbar")
+            <!-- Header-->
+           @include("partials.header")
+            <!-- Features section-->
+           @yield("contenu")
+        </main>
+        <!-- Footer-->
+       @include("partials.footer")
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="{{ asset('js/scripts.js') }}"></script>
+    </body>
 </html>
