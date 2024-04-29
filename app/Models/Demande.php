78995146e7896403dae8b0980256b041539ef27f
ivanlandry <58294595+ivanlandry@users.noreply.github.com>
2024-04-29 10:48:05 -0400

    demandes ok

--- app/Models/Demande.php
+++ app/Models/Demande.php
@@ -8,18 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Demande extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
+
     public function user(){
         return $this->belongsTo(User::class);
     }
 }
-=======
-
-    protected $fillable = ['telephone', 'email', 'statut', 'user_id'];
-
-    public function tuteur()
-    {
-        return $this->belongsTo(User::class, 'user_id');
-    }
-}
->>>>>>> 7d41cf973112384ab901e1bf8a8f2908d7cf6e25
